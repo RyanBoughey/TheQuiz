3ac192c58a6e52cc0dbfdf51e0a4f5e1050f41e9
Ryan Boughey <ryanboughey@MacBook-Pro-3.local>
2020-08-06 10:56:42 +0100

    created responce model, controller and migration

---
+++ app/Http/Controllers/ResponceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Responce;
+use Illuminate\Http\Request;
+
+class ResponceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Responce  $responce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Responce $responce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Responce  $responce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Responce $responce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Responce  $responce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Responce $responce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Responce  $responce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Responce $responce)
+    {
+        //
+    }
+}
